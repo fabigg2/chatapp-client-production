dc98f45ac04213c0240c5bc6861decce8c9c3b86
Fabian <cfgarcesg@gmail.com>
2022-02-17 08:56:22 -0500

    build directory

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path =  require('path');
 const app = express();
 const port =  process.env.PORT
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/*', (req, res)=>res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/*', (req, res)=>res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
 app.listen(port, ()=>console.log("server on port: "+port));
